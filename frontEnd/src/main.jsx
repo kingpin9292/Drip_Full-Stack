4e0de090f0777994249584f816063f7b0e5e3529
kingpin9292 <jakarea9292@gmail.com>
2026-06-02 23:12:25 -0400

    Implement Hero Page

--- frontEnd/src/main.jsx
+++ frontEnd/src/main.jsx
@@ -2,9 +2,11 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App.jsx";
-
+import ShopContextProvider from "./context/SHopContext.jsx";
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-    <App />
+    <ShopContextProvider>
+      <App />
+    </ShopContextProvider>
   </BrowserRouter>,
 );
